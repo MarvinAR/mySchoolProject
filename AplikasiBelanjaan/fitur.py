50e326675e2533a8028e84d43d0809b8c648a207
MarvinAR <marvin07ar@gmail.com>
2020-07-01 16:21:53 +0700

    Added admin mode

--- AplikasiBelanjaan/fitur.py
+++ AplikasiBelanjaan/fitur.py
@@ -8,34 +8,41 @@ import string
 
 fileData = 'data.json'
 fileUser = 'user.json'
+fileAdmin = 'admin.json'
 
 data = {}
 user = {}
+admin = {}
 
 def loadData():
 
-	global data, user
+	global data, user, admin
 
 	with open(fileData) as f:
 		data = load(f)
 	with open(fileUser) as f:
 		user = load(f)
+	with open(fileAdmin) as f:
+		admin = load(f)
 
 	return True
 
 def saveData():
 
-	global data, user
+	global data, user, admin
 
 	with open (fileData,"w") as f:
 		dump(data,f)
 	with open (fileUser,"w") as f:
 		dump(user,f)
+	with open (fileAdmin,"w") as f:
+		dump(admin,f)
 
 	return True
 
 def login():
 
+	system('cls')
 	print("Aplikasi Daftar Belanjaan\n")
 	counter = 1
 	Username = input('Masukkan Username : ')
@@ -72,6 +79,44 @@ def login():
 		print('Login Pass !')
 		return True
 
+def loginAdmin():
+
+	system('cls')
+	print("Aplikasi Daftar Belanjaan\n")
+	counter = 1
+	Username = input('Masukkan Username sebagai admin : ') 
+	Password = getpass('Masukkan Password sebagai admin : ')
+	dataCheck = False
+	passLogin = False  
+
+	if Username in admin:
+		dataCheck = True
+		passLogin = (admin[Username] == Password)
+	else:
+
+		dataCheck = False
+		passLogin = False
+
+	while not dataCheck or not passLogin:
+
+		counter += 1
+		if counter > 3:
+			return False
+		print('\nKombinasi Username dan Password salah\n')
+		Username = input('Masukkan Username sebagai admin : ')
+		Password = getpass('Masukkan Password sebagai admin : ')
+
+		if Username in admin:
+			dataCheck = True
+			passLogin = (admin[Username] == Password)
+
+		else:
+			dataCheck = False
+			passLogin = False 
+	else:
+		print('Login Pass !')
+		return True
+
 def print_menu():
 
 	print('Selamat datang \n')
@@ -83,6 +128,13 @@ def print_menu():
 	print("5. Lihat berdasarkan ID")
 	print("6. Lihat berdasarkan Nama")
 	'''
+	print("Q. Keluar\n")
+
+def menu_admin():
+	print("Selamat Datang \n")
+	print("1. Melihat User dan password")
+	print("2. Menghitung diskon")
+	print("3. Lihat total")
 	print("Q. Keluar\n")
 
 def tambah():
@@ -152,7 +204,7 @@ def lihat():
 			system("cls")
 			no = 1
 			print(f"Daftar belanja tanggal {hari} - {bulan} - {tahun} : \n")
-			print("| No | Nama Barang\t| Harga\t\t| Jumlah\t| Ex.Date\t| ID Barang\t|")
+			print("| No | Nama Barang\t| Harga\t\t| Jumlah\t| Ex.Date\t| ID Barang\t| Diskon \t\t\t\t|")
 			for info in data[date]:
 				barang = info
 				harga =  data[date][info]["harga"]
@@ -163,17 +215,17 @@ def lihat():
 				if ex_date > 12:
 					bulan_ex = int(bulan) - 12
 					tahun_ex = tahun + 1
-					print("=========================================================================================")
-					print(f"| {no}  | {barang}\t\t| {harga}\t\t| {jumlah}\t\t| {hari} - {bulan_ex} - {tahun_ex}\t| {ID}\t|")
+					print("====================================================================================================================================")
+					print(f"| {no}  | {barang}\t\t| {harga}\t\t| {jumlah}\t\t| {hari} - {bulan_ex} - {tahun_ex}\t| {ID}\t| Hanya dapat ditampilkan di mode admin\t|")
 					no += 1
 					saveData()
 				else:
 					bulan_ex = int(bulan) + 2
-					print("=========================================================================================")
-					print(f"| {no}  | {barang}\t\t| {harga}\t\t| {jumlah}\t\t| {hari} - {bulan_ex} - {tahun}\t| {ID}\t|")
+					print("=================================================================================================================================")
+					print(f"| {no}  | {barang}\t\t| {harga}\t\t| {jumlah}\t\t| {hari} - {bulan_ex} - {tahun}\t| {ID}\t| Hanya dapat ditampilkan di mode admin\t|")
 					no += 1
 					saveData()
-			print("=========================================================================================")
+			print("=================================================================================================================================")
 		else:
 			print(f"Anda belum mendaftarkan barang belanjaan anda di tanggal {hari} - {bulan} - {tahun}")
 			
@@ -217,6 +269,100 @@ def about():
 	print("Dibuat oleh Marvin AR dengan bimbingan dari Sir Anas")
 	print("Dibuat dari tanggal 23 Juni 2020")
 	print("Dalam membuat aplikasi ini, saya menggunakan aplikasi Sublime Text 3")
+
+def total():
+	system("cls")
+	if len(data) > 0:
+
+		hari =  input("Masukkan tanggal : ")
+		bulan = input("Masukkan bulan   : ")
+		tahun = input("Masukkan tahun   : ")
+		date = str(tahun) + str(bulan) + str(hari)
+		system('cls')
+		
+		if date in data:
+			print(f"Total harga barang barang di tanggal {hari} - {bulan} - {tahun} : \n")
+			for info in data[date]:
+				barang = info
+				harga =  data[date][info]["harga"]
+				jumlah = data[date][info]["kuantitas"]
+				A = int(harga) * int(jumlah)
+				print(f"{barang} : Rp {A},- ( {jumlah} buah )")
+
+def lihat_user():
+
+	system('cls')
+	print("Nama - nama user beserta passwordnya : \n")
+	print("| No | Nama User \t| Password \t|")
+	no = 1
+	for info in user:
+		nama_user = info
+		password  = user[info]
+		print("=========================================")
+		print(f"| {no}  | {nama_user}\t\t| {password}\t\t| ")
+		no += 1
+	print("=========================================")
+
+def diskon():
+
+	system("cls")
+	if len(data) > 0:
+
+		hari =  input("Masukkan tanggal : ")
+		bulan = input("Masukkan bulan   : ")
+		tahun = input("Masukkan tahun   : ")
+		date = str(tahun) + str(bulan) + str(hari)
+
+		
+		if date in data:
+			#Template header
+			system("cls")
+			no = 1
+			print(f"Daftar belanja tanggal {hari} - {bulan} - {tahun} : \n")
+			print("| No | Nama Barang\t| Harga\t\t| Jumlah\t|")
+			for info in data[date]:
+				barang = info
+				harga =  data[date][info]["harga"]
+				jumlah = data[date][info]["kuantitas"]
+				
+				print("=========================================================")
+				print(f"| {no}  | {barang}\t\t| {harga}\t\t| {jumlah}\t\t|")
+				no += 1
+					
+			print("=========================================================")
+
+			counter = 0
+			while counter != 1:
+				diskon = int(input("Masukkan jumlah diskon yang ingin dimasukkan ( ... % ) : "))
+				system('cls')
+				if diskon < 100:
+					counter +=1
+					print("| No | Nama Barang\t| Harga\t\t\t\t\t| Jumlah\t|")
+					for info in data[date]:
+						barang = info
+						harga =  data[date][info]["harga"]
+						jumlah = data[date][info]["kuantitas"]
+						diskon_dikurangi = 100 - diskon
+						harga_diskon = int(harga) * int(diskon_dikurangi) / 100
+						no = 1
+						print("=================================================================================")
+						print(f"| {no}  | {barang}\t\t| {harga_diskon} ( Di diskon {diskon}% )\t\t| {jumlah}\t\t|")
+						no += 1
+				
+					print("=================================================================================")
+				elif diskon > 100 or diskon < 0:
+					print("Mohon Masukkan angka dibawah 100 dan diatas 0")
+				else:
+					print("Mohon masukkan diskon dalam bentuk angka")
+
+		else:
+			print(f"Anda belum mendaftarkan barang belanjaan anda di tanggal {hari} - {bulan} - {tahun}")
+			
+	else:
+		print("Anda belum mendaftarkan barang belanjaan anda di aplikasi ini")
+
+				
+
 '''
 def lihat_berdasar_ID():
 
--- AplikasiBelanjaan/main.py
+++ AplikasiBelanjaan/main.py
@@ -9,45 +9,64 @@ system("cls")
 
 if statusLoading :
 
-	passlogin = fitur.login()
-	if passlogin :
-		print("Welcome")	
-		menuchoice = ""
+	role = input("Masukkan role anda ( Admin atau User ) : ")
+	if role.lower() == "user":
 
-		while menuchoice.lower() != 'q':
-			system('cls')
-			fitur.print_menu()
-			menuchoice = input('Masukkan opsi anda : ').lower()
+		passlogin = fitur.login()
+		if passlogin :
+			print("Welcome")	
+			menuchoice = ""
 
-			if menuchoice == '1':
-				fitur.tambah()
-				input('\nKlik tombol [ ENTER ] untuk kembali ke menu ')
-			elif menuchoice == '2':
-				fitur.lihat()
-				input('\nKlik tombol [ ENTER ] untuk kembali ke menu ')
-			elif menuchoice == '3':
-				fitur.remove()
-				input('\nKlik tombol [ ENTER ] untuk kembali ke menu ')
-			elif menuchoice == '4':
-				fitur.about()
-				input('\nKlik tombol [ ENTER ] untuk kembali ke menu ')
-				'''
-			elif menuchoice == '5':
-				fitur.lihat_berdasar_ID()
-				input('\nKlik tombol [ ENTER ] untuk kembali ke menu ')
-			elif menuchoice == '6':
-				fitur.lihat_berdasar_nama()
-				input('\nKlik tombol [ ENTER ] untuk kembali ke menu ')
-				'''			
-			elif menuchoice.lower() == 'q':
-				system("cls")
-				print("Terima kasih telah menggunakan aplikasi ini")
-				sleep(2)
-				system("cls")
-				break
-			else :
-				print("Masukkan pilihan dengan benar")
-				input('\nKlik tombol [ ENTER ] untuk kembali ke menu :)')
+			while menuchoice.lower() != 'q':
+				system('cls')
+				fitur.print_menu()
+				menuchoice = input('Masukkan opsi anda : ').lower()
+
+				if menuchoice == '1':
+					fitur.tambah()
+					input('\nKlik tombol [ ENTER ] untuk kembali ke menu ')
+				elif menuchoice == '2':
+					fitur.lihat()
+					input('\nKlik tombol [ ENTER ] untuk kembali ke menu ')
+				elif menuchoice == '3':
+					fitur.remove()
+					input('\nKlik tombol [ ENTER ] untuk kembali ke menu ')
+				elif menuchoice == '4':
+					fitur.about()
+					input('\nKlik tombol [ ENTER ] untuk kembali ke menu ')
+				elif menuchoice.lower() == 'q':
+					system("cls")
+					print("Terima kasih telah menggunakan aplikasi ini")
+					sleep(2)
+					system("cls")
+					break
+				else :
+					print("Masukkan pilihan dengan benar")
+					input('\nKlik tombol [ ENTER ] untuk kembali ke menu :)')
+
+	elif role.lower() == "admin":
+
+		passlogin = fitur.loginAdmin()
+		if passlogin :
+			print("Welcome")	
+			menuchoice = ""
+
+			while menuchoice.lower() != 'q':
+				system('cls')
+				fitur.menu_admin()
+				menuchoice = input('Masukkan opsi anda : ').lower()
+				if menuchoice == '1':
+					fitur.lihat_user()
+					input('\nKlik tombol [ ENTER ] untuk kembali ke menu ')
+				elif menuchoice == '2':
+					fitur.diskon()
+					input('\nKlik tombol [ ENTER ] untuk kembali ke menu ')
+				elif menuchoice == '3':
+					fitur.total()
+					input('\nKlik tombol [ ENTER ] untuk kembali ke menu ')
+				else :
+					print("Masukkan pilihan dengan benar")
+					input('\nKlik tombol [ ENTER ] untuk kembali ke menu :)')
 				
 	else:
 		print("\nGagal untuk Log In")
